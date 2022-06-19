19574b07b0c7dfc32fe3a8a8e23fa992304ced11
SuperStrain <1143124497@qq.com>
2022-06-19 23:59:41 +0800

    6月19日 异步爬取数据基本了解，同时使用异步爬取，爬取B站评论信息

--- 51job/job.py
+++ 51job/job.py
@@ -9,6 +9,8 @@
 import urllib.request,urllib.error
 from urllib import parse
 import re
+import json
+import chardet
 
 def main():
     getData()
@@ -16,7 +18,8 @@ def main():
 #设置url链接
 def setURL(keywords,page):
     keywords=parse.quote(parse.quote(keywords))
-    url = 'https://search.51job.com/list/000000,000000,0000,00,9,99,' + keywords + ',2,'+str(page)+'.html?lang=c&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&ord_field=0&dibiaoid=0&line=&welfare='
+    url = 'https://search.51job.com/list/040000,000000,0000,00,9,99,' + keywords + ',2,'+str(page)+'.html?lang=c&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&ord_field=0&dibiaoid=0&line=&welfare='
+    print(url)
     return url
 
 
@@ -26,19 +29,54 @@ def askURL(url):
     request=urllib.request.Request(url,headers=head)
     response=urllib.request.urlopen(request)
     html=''
-    html=response.read().decode('utf-8')
+    html=response.read().decode('gbk')
     # print(html)
     return html
 
-#获取多个网页信息并进行解析
+# 获取多个网页信息并进行解析
 def getData():
     keywords=input('请输入你想了解的职位的关键字：')
     datalist=[]
-    for i in range(0,1):
+    for i in range(0,3):
         url=setURL(keywords,i+1)
         html=askURL(url)
         data=re.findall(r"\"engine_jds\":(.+?),\"jobid_count\"",html)
-        print(data)
+        jsonobj=json.loads(data[0])
+        for item in jsonobj:
+            data=[]
+            data.append(item['job_name'])
+            data.append(item['company_name'])
+            data.append(item['companytype_text'])
+            data.append(item['providesalary_text'])
+            data.append(item['workarea_text'])
+            data.append(item['jobwelf'])#工作的详细情况
+            data.append(item['updatedate'])#发布时间
+            datalist.append(data)
+    print(datalist)
+    return datalist
+
+# 从本地的html解析数据
+# def getData():
+#     file=open('../text/job.html','rb')
+#     html=file.read().decode('gbk')
+#     datalist=[]
+#
+#     data=re.findall(r"\"engine_jds\":(.+?),\"jobid_count\"",html)
+#     jsonobj=json.loads(data[0])
+#     for item in jsonobj:
+#         data=[]
+#         data.append(item['job_name'])
+#         data.append(item['company_name'])
+#         data.append(item['companytype_text'])
+#         data.append(item['providesalary_text'])
+#         data.append(item['workarea_text'])
+#         data.append(item['jobwelf'])#工作的详细情况
+#         data.append(item['updatedate'])#发布时间
+#         datalist.append(data)
+#
+#     print(datalist)
+#     return datalist
+
 
 
 
---
+++ bilibiliRemark/remark.py
@@ -0,0 +1,14 @@
+# -*- codeing= utf-8 -*-
+# @Time : 2022/5/15 11:12
+# @Author : Yina
+# @File : f.py
+# @Software: PyCharm
+
+# 'https://api.bilibili.com/x/v2/reply/main?csrf=4cb2f8e3ab70964bf94bd639ba239832&mode=3&next=2&oid=927086822&plat=1&type=1'
+# 'https://api.bilibili.com/x/v2/reply/main?csrf=4cb2f8e3ab70964bf94bd639ba239832&mode=3&next=4&oid=927086822&plat=1&type=1'
+# 'https://api.bilibili.com/x/v2/reply/main?csrf=4cb2f8e3ab70964bf94bd639ba239832&mode=3&next=3&oid=927086822&plat=1&type=1'
+
+def main():
+    baseurl='https://api.bilibili.com/x/v2/reply/main?csrf=4cb2f8e3ab70964bf94bd639ba239832&mode=3&next=1&oid=927086822&plat=1&type=1'
+
+
