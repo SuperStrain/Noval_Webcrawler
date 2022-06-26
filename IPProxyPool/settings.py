d27f948f1c6f39a23ba7c5f33cd7690d85c13d7c
SuperStrain <1143124497@qq.com>
2022-06-27 00:06:01 +0800

    6月27日 完成log.py（日志）设置

--- IPProxyPool/settings.py
+++ IPProxyPool/settings.py
@@ -6,3 +6,11 @@
 
 #定义MAX_SCORE=50,表示代理IP的默认最高分数
 MAX_SCORE=50
+
+#日志的配置信息
+import logging
+#默认的配置
+LOG_LEVEL=logging.INFO  #默认等级
+LOG_FMT='%(asctime)s %(filename)s [line:%(lineno)d] %(levelname)s: %(message)s'
+LOG_DATEFMT='%Y-%m-%d %H:%M:%S'#默认时间格式
+LOG_FILENAME='log.log'#默认日志文件名称
